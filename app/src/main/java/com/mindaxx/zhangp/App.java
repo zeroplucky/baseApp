681e6478b84aa96bb19bb620a451916ba94dbc1d
Zero <1135221105@qq.com>
2019-10-16 17:46:03 +0800

    集成腾讯bugly

--- app/src/main/java/com/mindaxx/zhangp/App.java
+++ app/src/main/java/com/mindaxx/zhangp/App.java
@@ -4,7 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.minda.logger.AndroidLogAdapter;
+import com.minda.logger.DiskLogAdapter;
+import com.minda.logger.Logger;
 import com.mindaxx.zhangp.util.SpUtil;
+import com.mindaxx.zhangp.util.UncaughtExceptionHandlerImpl;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 
 
 /**
@@ -23,5 +29,38 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         SpUtil.init(getApplicationContext());
+        initBugly();
+        initLogger();
+        initCrashThrowable();
     }
+
+    private void initBugly() {
+        Beta.autoInit = true;
+        Beta.initDelay = 1 * 1000;
+        Beta.autoCheckUpgrade = true;
+        Bugly.init(getApplicationContext(), "3699d7411b", false);
+    }
+
+    /*
+     * 日志
+     * */
+    private void initLogger() {
+        Logger.addLogAdapter(new AndroidLogAdapter());
+        //Logger.addLogAdapter(new DiskLogAdapter(getApplicationContext()));
+    }
+
+    /*
+     * 捕获异常
+     * */
+    private void initCrashThrowable() {
+        final Context applicationContext = getApplicationContext();
+        UncaughtExceptionHandlerImpl.getInstance().init(applicationContext, MainActivity.class,
+                new UncaughtExceptionHandlerImpl.OnCrashBack() {
+                    @Override
+                    public void onBack(Throwable ex, String result) {
+                        Logger.logWithTag("throwable-123", result, ex);
+                    }
+                });
+    }
+
 }
