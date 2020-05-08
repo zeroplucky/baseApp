636ad2e8b6f2a91a1414ddc760dba75d7b2a07aa
Zero <p421123@gmail.com>
2020-05-08 10:03:31 +0800

    添加 下来选择pop

--- app/src/main/java/com/mindaxx/zhangp/ui/activity/LoginActivity.java
+++ app/src/main/java/com/mindaxx/zhangp/ui/activity/LoginActivity.java
@@ -3,16 +3,20 @@ package com.mindaxx.zhangp.ui.activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.gyf.barlibrary.ImmersionBar;
+import com.lxj.xpopup.XPopup;
 import com.mindaxx.zhangp.MainActivity;
 import com.mindaxx.zhangp.R;
 import com.mindaxx.zhangp.base.BaseMvpActivity;
 import com.mindaxx.zhangp.http.URL2;
+import com.mindaxx.zhangp.util.SizeUtils;
 import com.mindaxx.zhangp.util.SpUtil;
+import com.mindaxx.zhangp.widget.CustomPopupView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -67,6 +71,18 @@ public class LoginActivity extends BaseMvpActivity {
                 Toast.makeText(getApplicationContext(), URL2.BASE_URL, Toast.LENGTH_LONG).show();
             }
         }).setNegativeButton("取消", null).show();
+    }
+
+    // 类淘宝下来选择
+    private void popupWindown(View op, String[] content) {
+        new XPopup.Builder(this)
+                .atView(op).offsetY(SizeUtils.px2dp(1))
+                .asCustom(new CustomPopupView(mContext, op, content, new CustomPopupView.OnSelectListener2() {
+                    @Override
+                    public void onSelect(int id, String data) {
+                        Toast.makeText(mContext, "  " + id + " . " + data, Toast.LENGTH_SHORT).show();
+                    }
+                })).show();
     }
 
 }
---
+++ app/src/main/java/com/mindaxx/zhangp/widget/CustomPopupView.java
@@ -0,0 +1,91 @@
+package com.mindaxx.zhangp.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.lxj.easyadapter.EasyAdapter;
+import com.lxj.easyadapter.ViewHolder;
+import com.lxj.xpopup.impl.PartShadowPopupView;
+import com.lxj.xpopup.widget.VerticalRecyclerView;
+import com.mindaxx.zhangp.R;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class CustomPopupView extends PartShadowPopupView {
+
+    private int width;
+    private int height;
+    VerticalRecyclerView recyclerView;
+    private HashMap<Integer, String> map = new HashMap<>();
+
+    public CustomPopupView(@NonNull Context context, View view, String[] data, OnSelectListener2 selectListener) {
+        super(context);
+        this.selectListener = selectListener;
+        this.data = data;
+        width = view.getWidth();
+        map.clear();
+    }
+
+    @Override
+    protected int getImplLayoutId() {
+        return R.layout.xpopup_attach_impl_list;
+    }
+
+
+    @Override
+    protected int getMaxWidth() {
+        return width;
+    }
+
+    @Override
+    protected int getMaxHeight() {
+        return 600;
+    }
+
+    @Override
+    protected void initPopupContent() {
+        super.initPopupContent();
+        recyclerView = (VerticalRecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutParams(new LinearLayout.LayoutParams(width, -2));
+        recyclerView.setupDivider();
+        final EasyAdapter<String> adapter = new EasyAdapter<String>(Arrays.asList(data), R.layout.xpopup_adapter_text2) {
+            @Override
+            protected void bind(@NonNull ViewHolder holder, @NonNull final String s, final int position) {
+                holder.setText(R.id.tv_text, s);
+
+                holder.itemView.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dismiss();
+                        if (selectListener != null) {
+                            selectListener.onSelect(position, data[position]);
+                        }
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    String[] data;
+
+    private OnSelectListener2 selectListener;
+
+    public CustomPopupView setOnSelectListener(OnSelectListener2 selectListener) {
+        this.selectListener = selectListener;
+        return this;
+    }
+
+    public interface OnSelectListener2 {
+        void onSelect(int id, String data);
+    }
+
+    @Override
+    protected Drawable getPopupBackground() {
+        return super.getPopupBackground();
+    }
+}
